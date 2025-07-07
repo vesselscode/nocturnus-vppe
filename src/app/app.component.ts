101bde9e9510831906193388706c9a62317677fd
vesselscode <brenoraphaeldel@gmail.com>
2025-07-07 14:08:08 -0300

    CONCEPT1-001: adjust to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EngineComponent } from './Engine/Engine.component';
+import { EengineComponent } from './eengine/eengine.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    EngineComponent
+    EengineComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/eengine/eengine.component.ts
@@ -0,0 +1,56 @@
+import { Component } from "@angular/core";
+import { NgIf } from "@angular/common";
+
+@Component({
+    selector:'app-eengine',
+    standalone: true,
+    templateUrl: './eengine.component.html',
+    styleUrls: ['./eengine.component.scss'],
+    imports: [NgIf]
+})
+
+export class EengineComponent{
+
+    public pistonPosition: String = "down";
+    public eengineIntegrity = 100;
+    public eengineStatus = ""
+    public generatedEnergy: number = 0; // Energia gerada pelo pistão
+    public pistonUseDecreaser: number = 3; // deve subtrair do eengineIntegrity a cada uso do pistão
+
+
+    constructor(){
+        console.log("construtor");
+        console.log(this.integrity());
+    }
+
+    public pistonUp(){
+        this.pistonPosition = "up";
+        console.log("Piston goes up");
+    }
+
+    public pistonDown(){
+        this.pistonPosition = "down";
+        console.log("Piston goes down");
+    }
+
+    public integrity(){
+        if(this.eengineIntegrity === 0){
+            this.eengineStatus = "Engine is broken, the vampire escaped!";
+        }
+        else if(this.eengineIntegrity < 50){
+            this.eengineStatus = "Engine is damaged, it needs repair!";
+        }
+        
+        this.eengineStatus = "Engine is in good condition.";
+        
+    } 
+
+    // public pistonUse(){
+    //     if (this.pistonPosition === "up") {
+    //         this.generatedEnergy += 1; // Gera energia ao mover o pistão para baixo
+    //         this.eengineIntegrity -= this.pistonUseDecreaser; // Diminui a integridade do motor
+    //         console.log(`Energy generated: ${this.generatedEnergy} units`);
+    //         console.log(`Engine integrity: ${this.eengineIntegrity}`);
+    //     }
+    // }
+}
